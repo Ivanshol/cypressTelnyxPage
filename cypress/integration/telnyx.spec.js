0319de060cf23ab94b9ac79acb01435dca6da390
Ivan Sholomitskyi <102619203+Ivanshol@users.noreply.github.com>
2022-06-22 14:55:23 +0300

    Update telnyx.spec.js

--- cypress/integration/telnyx.spec.js
+++ cypress/integration/telnyx.spec.js
@@ -3,7 +3,6 @@
 const { should } = require("chai")
 import TelnyxPage from './page/TelnyxPage';
 import TelnyxSignUpPage from './page/TelnyxSignUpPage';
-import TelnyxContactUsPage from './page/TelnyxContactUsPage';
 import TelnyxFooter from './page/TelnyxFooter';
 import footerProductLinks from './page/TelnyxFooter';
 import footerPricingLinks from './page/TelnyxFooter';
@@ -61,7 +60,7 @@ context('Telnyx', () => {
       describe('Redmine website SignUp page', () => {
         it('Should test bottom mission control menu Sign Up', () => {
           TelnyxFooter.clickFooterLinkSingle('sign-up');
-          cy.get('[class="Text-sc-5o8owa-0 sc-1e9319a3-4 gkWopw gjiitA"]').contains('Create a free account');
+          cy.get('body div h1').contains('Create a free account');
         })
 
         it('Should negative test Sign Up function with empty inputs', () => {
@@ -73,14 +72,14 @@ context('Telnyx', () => {
 
         it('Should negative test Sign Up function with correct inputs but without password', () => {
           TelnyxFooter.clickFooterLinkSingle('sign-up');
-          TelnyxSignUpPage.inputEmailData(`shivantesting@gmail.com`);
+          TelnyxSignUpPage.inputEmailData('shivantesting@gmail.com');
           TelnyxSignUpPage.inputNameData('name');
           TelnyxSignUpPage.pressCreateAccountButton();
           cy.get('#password_requirements').should('be.visible');
         })
 
         it('Should negative test Sign Up function with incorrect email input data', () => {
-          TelnyxFooter.clickFooterLinkSingle('sign-up');
+          cy.get('footer [href="/sign-up"]').click();
           TelnyxSignUpPage.inputIncorrectEmailData()
           TelnyxSignUpPage.inputNameData('name');
           TelnyxSignUpPage.pressCreateAccountButton();
@@ -91,39 +90,40 @@ context('Telnyx', () => {
 
       describe('Telnyx middle page buttons', () => {
         it('Should test Messaging API button', () => {
-          TelnyxPage.scrollMidPageButtonIntoView(0)
-          TelnyxPage.clickMidPageButton(0)
-          cy.get(' [class="sc-a7a16c36-6 bZeRhp"] span').contains('SMS API');
+          TelnyxPage.scrollMidPageButtonIntoView(1)
+          TelnyxPage.clickMidPageButton(1)
+          cy.get('body h1').contains('SMS API');
        })
 
         it('Should test Video API button', () => {
-          TelnyxPage.scrollMidPageButtonIntoView(1)
-          TelnyxPage.clickMidPageButton(1)
-          cy.get(' [class="sc-a7a16c36-6 bZeRhp"] span').contains('Build an integrated video solution');
+          TelnyxPage.scrollMidPageButtonIntoView(2)
+          TelnyxPage.clickMidPageButton(2)
+          cy.get('body h1').contains('Build an integrated video solution');
        })
 
         it('Should test Wireless button', () => {
-          TelnyxPage.scrollMidPageButtonIntoView(2)
-          TelnyxPage.clickMidPageButton(2)
-          cy.get(' [class="sc-a7a16c36-6 bZeRhp"] span').contains('IoT SIM Cards');
+          TelnyxPage.scrollMidPageButtonIntoView(3)
+          TelnyxPage.clickMidPageButton(3)
+          cy.get('body h1').contains('IoT SIM Cards');
       })
 
         it('Should test Voice API button', () => {
-          TelnyxPage.scrollMidPageButtonIntoView(3)
-          TelnyxPage.clickMidPageButton(3)
-          cy.get(' [class="sc-a7a16c36-6 bZeRhp"] span').contains('Voice API');
+          TelnyxPage.scrollMidPageButtonIntoView(4)
+          TelnyxPage.clickMidPageButton(4)
+          cy.get('body h1').contains('Voice API');
       })
 
         it('Should test Elastic SIP Trunk button', () => {
-          TelnyxPage.scrollMidPageButtonIntoView(4)
-          TelnyxPage.clickMidPageButton(4);
-          cy.get('[class="sc-a7a16c36-6 bZeRhp"] span').contains('SIP Trunks for ');
+          TelnyxPage.scrollMidPageButtonIntoView(5)
+          TelnyxPage.clickMidPageButton(5);
+          cy.get('body h1').contains('SIP Trunks for ');
       })
 
         it('Should test Storage button', () => {
-          TelnyxPage.scrollMidPageButtonIntoView(5)
-          TelnyxPage.clickMidPageButton(5)
-          cy.get(' [class="sc-a7a16c36-6 bZeRhp"] span').contains('Better cloud storage,');
+          TelnyxPage.scrollMidPageButtonIntoView(6)
+          TelnyxPage.clickMidPageButton(6)
+          cy.get('body h1').contains('Better cloud storage,');
       })
       })
 })
+
